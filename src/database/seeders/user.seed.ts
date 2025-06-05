75a6bf77756f8b4797c348d2a8958586f96a5a21
oligregz <gregorysak90@gmail.com>
2025-06-05 15:45:32 -0300

    refact: remove incorrect code

--- src/database/seeders/user.seed.ts
+++ src/database/seeders/user.seed.ts
@@ -6,8 +6,6 @@ export class UserSeeder {
   constructor(private readonly userService: UserService) {}
 
   async seed() {
-		const NUMBER_OF_USERS = 50;
-    const usersToSeed = 'createDummyUsers(NUMBER_OF_USERS)';
-    const createdUser = await this.userService.signup(user);
+    // ToDo
   }
 }
