b6a6336779ce7e2785b7b82afb0c421aa7b89378
oligregz <gregorysak90@gmail.com>
2025-07-08 15:42:59 -0300

    refact: change name function

--- src/services/utils/getRandomVerifyCode.util.ts
+++ src/services/utils/getRandomVerifyCode.util.ts
@@ -1,6 +1,6 @@
 import { getOneRandomNumber } from "../../database/seeders/utils/createDummyUsers.util";
 
-export function getRandomNumber(): string {
+export function getRandomCode(): string {
 	return Array.from({ length: 4 }, () =>
 		getOneRandomNumber(),
 	)
